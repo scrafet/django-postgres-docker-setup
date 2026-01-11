11709b38b642be093e90fd707cc56f4633a2faeb
scrafet <scrafet@gmail.com>
2026-01-10 22:03:48 -0500

    UI Final: Dashboard con DataTables, cabeceras estilizadas e iconos

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-    return render(request, 'inicio.html')
+    # Esta es la "fuente de datos" manual (Pythonic way sin DB por ahora)
+    datos_servicios = [
+        {"id": 1, "nombre": "Contenedor Django", "estado": "Activo", "uso_cpu": "12%"},
+        {"id": 2, "nombre": "PostgreSQL DB", "estado": "Activo", "uso_cpu": "5%"},
+        {"id": 3, "nombre": "Redis Cache", "estado": "Inactivo", "uso_cpu": "0%"},
+        {"id": 4, "nombre": "Nginx Proxy", "estado": "Error", "uso_cpu": "98%"},
+        {"id": 5, "nombre": "Celery Worker", "estado": "Activo", "uso_cpu": "15%"},
+    ]
+    
+    # El tercer argumento {'servicios': datos_servicios} es el "contexto"
+    # que viaja hacia el HTML
+    return render(request, 'inicio.html', {'servicios': datos_servicios})
